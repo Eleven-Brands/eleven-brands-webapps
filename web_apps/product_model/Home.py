178eba09d3126b7cd0d0e75c838215597d281fb4
Gustavo Alves <gustavo.ttaa97@gmail.com>
2025-05-29 18:37:06 -0300

    feat(Home): Added User Authentication - imported the require_login and show_user_sidebar to Home page

--- web_apps/product_model/Home.py
+++ web_apps/product_model/Home.py
@@ -1,5 +1,8 @@
 import streamlit as st
+from shared.shared_auth import require_login, show_user_sidebar
+
 
 st.set_page_config(page_title="Product Model - Home", layout="wide")
 
-st.title("Home")
+user = require_login()
+show_user_sidebar(user)
