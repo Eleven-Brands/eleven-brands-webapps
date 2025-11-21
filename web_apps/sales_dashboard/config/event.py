fadeca75f6ce2d951a2d628e86b5c03adb861b26
Gustavo Alves <gustavo.ttaa97@gmail.com>
2025-11-21 12:08:34 -0300

    chore(event): update event dates

--- web_apps/sales_dashboard/config/event.py
+++ web_apps/sales_dashboard/config/event.py
@@ -10,10 +10,20 @@ for use throughout the app.
 
 import datetime
 
-EVENT_NAME = "Prime Day"
+EVENT_NAME = "Black Friday/Cyber Monday"
 EVENT_DATES = {
-    "PD - Day 1": datetime.date(2025, 7, 8),
-    "PD - Day 2": datetime.date(2025, 7, 9),
-    "PD - Day 3": datetime.date(2025, 7, 10),
-    "PD - Day 4": datetime.date(2025, 7, 11),
+    "PD - Day 1": datetime.date(2025, 11, 20),
+    "PD - Day 2": datetime.date(2025, 11, 21),
+    "PD - Day 3": datetime.date(2025, 11, 22),
+    "PD - Day 4": datetime.date(2025, 11, 23),
+
+    "PD - Day 5": datetime.date(2025, 11, 24),
+    "PD - Day 6": datetime.date(2025, 11, 25),
+    "PD - Day 7": datetime.date(2025, 11, 26),
+    "PD - Day 8": datetime.date(2025, 11, 27),
+
+    "PD - Day 9": datetime.date(2025, 11, 28),
+    "PD - Day 10": datetime.date(2025, 11, 29),
+    "PD - Day 11": datetime.date(2025, 11, 30),
+    "PD - Day 12": datetime.date(2025, 12, 1),
 }
